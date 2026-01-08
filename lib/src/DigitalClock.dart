d3c760d919a964588f3124a5991d66d3233bb6a1
Aurangzeb Husain <aurhusai@WKMZTCA1B0EE.global.publicisgroupe.net>
2026-01-08 09:46:04 +0530

    added dynamic text style

--- lib/src/DigitalClock.dart
+++ lib/src/DigitalClock.dart
@@ -15,16 +15,17 @@ class DigitalClock extends StatefulWidget {
   final EdgeInsets? padding;
   final bool isLive;
   final double textScaleFactor;
+  final TextStyle? textStyle;
 
   ///You can pass INTL date format skeleton here, to choose in what format you want to display the time. Note in case of ui distored please wrap it inside a container with desired padding and margin.
   ///For more info about skeletons please refer to this site https://pub.dev/documentation/intl/latest/intl/DateFormat-class.html
   final String? format;
 
-  const DigitalClock({this.format, this.datetime, this.showSeconds = true, this.decoration, this.padding, this.digitalClockTextColor = Colors.black, this.textScaleFactor = 1.0, isLive, Key? key})
+  const DigitalClock({this.format, this.datetime, this.showSeconds = true, this.decoration, this.padding, this.digitalClockTextColor = Colors.black, this.textScaleFactor = 1.0, this.textStyle, isLive, Key? key})
       : this.isLive = isLive ?? (datetime == null),
         super(key: key);
-  const DigitalClock.dark({this.format, this.datetime, this.showSeconds = true, this.padding, this.decoration = const BoxDecoration(color: Colors.black, shape: BoxShape.rectangle, borderRadius: BorderRadius.all(Radius.circular(15))), this.digitalClockTextColor = Colors.white, this.textScaleFactor = 1.0, this.isLive = false, Key? key}) : super(key: key);
-  const DigitalClock.light({this.format, this.datetime, this.showSeconds = true, this.padding, this.decoration = const BoxDecoration(color: Colors.white, shape: BoxShape.rectangle, borderRadius: BorderRadius.all(Radius.circular(15))), this.digitalClockTextColor = Colors.black, this.textScaleFactor = 1.0, this.isLive = false, Key? key}) : super(key: key);
+  const DigitalClock.dark({this.format, this.datetime, this.showSeconds = true, this.padding, this.decoration = const BoxDecoration(color: Colors.black, shape: BoxShape.rectangle, borderRadius: BorderRadius.all(Radius.circular(15))), this.digitalClockTextColor = Colors.white, this.textScaleFactor = 1.0, this.textStyle, this.isLive = false, Key? key}) : super(key: key);
+  const DigitalClock.light({this.format, this.datetime, this.showSeconds = true, this.padding, this.decoration = const BoxDecoration(color: Colors.white, shape: BoxShape.rectangle, borderRadius: BorderRadius.all(Radius.circular(15))), this.digitalClockTextColor = Colors.black, this.textScaleFactor = 1.0, this.textStyle, this.isLive = false, Key? key}) : super(key: key);
 
   @override
   _DigitalClockState createState() => _DigitalClockState(datetime);
@@ -75,7 +76,7 @@ class _DigitalClockState extends State<DigitalClock> {
       child: Container(
           constraints: BoxConstraints(minWidth: widget.showSeconds ? 110 * widget.textScaleFactor : 85.0 * widget.textScaleFactor, minHeight: 20.0 * widget.textScaleFactor),
           child: CustomPaint(
-            painter: DigitalClockPainter(format: widget.format, showSeconds: widget.showSeconds, datetime: datetime, digitalClockTextColor: widget.digitalClockTextColor, textScaleFactor: widget.textScaleFactor),
+            painter: DigitalClockPainter(textStyle: widget.textStyle, format: widget.format, showSeconds: widget.showSeconds, datetime: datetime, digitalClockTextColor: widget.digitalClockTextColor, textScaleFactor: widget.textScaleFactor),
           )),
     );
   }
